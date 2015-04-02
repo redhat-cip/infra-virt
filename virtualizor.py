redhat-cip/infra-virt

commit 6a39b9c199925856f0db83038fc31f80a081441a

Specifing the PXE interface from the YAML
The goal is to ensure we can use specific interface for pxe boot,
before we are assuming it was the first, but it's not always the case.

virtualizor.py
@@ -267,7 +267,6 @@ class Host(object):
             cloud_init_image = self._create_cloud_init_image()
             self._register_disk(cloud_init_image)
 
-        self.meta['nics'][0]['boot_order'] = 2
         self.meta['disks'][0]['boot_order'] = 1
 
     def _create_cloud_init_image(self):
@@ -449,10 +448,14 @@ class Host(object):
         nic.setdefault('gateway', '')
         nic.setdefault('nat', False)
         nic.setdefault('vlan', False)
+        nic.setdefault('pxe', False)
         if nic['network_name'] == '__public_network__':
             nic['network_name'] = self.conf.public_network
         if nic['ip']:
             nic['bootproto'] = 'static'
+        if nic['pxe'] == True:
+            nic['boot_order'] = 2
+
         self.meta['nics'].append(nic)
 
     def dump_libvirt_xml(self):

commit db7be85078bb40b9ae2ab0ae2ee2b7a47e917dd7

pep8: use 'is True' instead of '== True'
Regression introduced by 6a39b9c199925856f0db83038fc31f80a081441a.

virtualizor.py
@@ -464,7 +464,7 @@ class Host(object):
             nic['network_name'] = self.conf.public_network
         if nic['ip']:
             nic['bootproto'] = 'static'
-        if nic['pxe'] == True:
+        if nic['pxe'] is True:
             nic['boot_order'] = 2
 
         self.meta['nics'].append(nic)